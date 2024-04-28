cb2f1367634fd8cbc291dced464fb5379b0f618b
VIGNESH30031441 <146426762+VIGNESH30031441@users.noreply.github.com>
2024-04-29 02:40:47 +0530

    Updated

--- 18.04_nodeJs/index.js
+++ 18.04_nodeJs/index.js
@@ -3,3 +3,16 @@ const { v4 }=require('uuid')
 
 console.log(format(new Date(), 'ddMMyyyy\tHH:mm:ss'))
 console.log(v4())
+
+const logEvents=require('./logEvents')
+
+const EventEmitter = require('node:events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('log', (msg) => {
+  logEvents(msg);
+});
+myEmitter.emit('log',"success");
